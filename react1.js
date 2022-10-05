713e8d2cd88b64a4c6ccfbc03525373dc09b66d0
Casey Warren <caseywar1017@gmail.com>
2022-10-05 16:07:36 -0400

    sample code from pluralsite

---
+++ react1.js
@@ -0,0 +1,15 @@
+  function Button() {
+    const [counter, setCounter] = useState(0);
+    const handleClick = () => setCounter(counter+1);
+    
+    return (
+      <button onClick={handleClick}>
+        {counter}
+      </button>
+      );
+  };
+  
+  ReactDOM.render(
+    <Button />, 
+    document.getElementById('mountNode'),
+  );
